fe11be0fb4aa7ce943b3b4b993fd3d788e9f5415
Daniel Armas Ramírez <50346756+daniel692a@users.noreply.github.com>
2022-06-28 19:37:06 -0500

    Time: 23 ms (87.60%), Space: 23.2 MB (40.47%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int i  =0;
-        int j = s.size()-1;
-        while(i<=j){
+        int i  =0; // -> O(1)
+        int j = s.size()-1; //-> O(1)
+        while(i<=j){ // -> O(n)
             swap(s[i], s[j]);
             i++; j--;
         }
