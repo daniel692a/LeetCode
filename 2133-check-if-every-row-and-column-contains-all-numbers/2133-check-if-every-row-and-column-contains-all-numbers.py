a4ed428476c34613d81265678e2b2e2adf19a387
Daniel Armas Ramírez <50346756+daniel692a@users.noreply.github.com>
2022-08-11 20:27:24 -0500

    Time: 1322 ms (43.63%), Space: 14.4 MB (51.84%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -3,18 +3,19 @@ class Solution:
         row_set = set() # O(n)
         column_set = set() # O(n)
         
-        ## check columns
         for i in range(len(matrix)): #O(n^3)
+            # check rows
             row_set = set(matrix[i])
             if len(row_set) != len(matrix):
                 return False
             row_set.clear()
-            for x in range(len(matrix)):
-                for j in range(len(matrix)):
-                    column_set.add(matrix[j][x])
-                if len(column_set)!=len(matrix):
-                    return False
-                column_set.clear()
-
+            ## check columns
+            if i==0:
+                for x in range(len(matrix)):
+                    for j in range(len(matrix)):
+                        column_set.add(matrix[j][x])
+                    if len(column_set)!=len(matrix):
+                        return False
+                    column_set.clear()
         return True
         
