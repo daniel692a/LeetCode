3cca45f115911295712cc79830a37ed1668f18be
Daniel Armas Ramírez <50346756+daniel692a@users.noreply.github.com>
2022-08-15 14:07:06 -0500

    Time: 43 ms (64.07%), Space: 13.8 MB (64.15%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,19 +1,24 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        
         if len(needle) == 0:
             return 0
         elif len(needle)>len(haystack):
             return -1
-        n_ptr, h_ptr = 0, 0
         
-        for i in range(len(haystack)):
+        n_ptr, h_ptr = 0, 0 # O(1)
+        
+        def isTheSame(a, b) -> bool:
+            return a==b
+        
+        for i in range(len(haystack)): # O(n)
             
             if haystack[i]==needle[0]:
                 h_ptr = i
                 while n_ptr<len(needle):
-                    if (h_ptr < len(haystack) and (n_ptr == len(needle)-1) and (needle[n_ptr] == haystack[h_ptr])):
+                    if (h_ptr < len(haystack) and (n_ptr == len(needle)-1) and isTheSame(needle[n_ptr], haystack[h_ptr])):
                         return i
-                    elif ((h_ptr < len(haystack)) and (needle[n_ptr] == haystack[h_ptr])):
+                    elif ((h_ptr < len(haystack)) and isTheSame(needle[n_ptr], haystack[h_ptr])):
                         h_ptr+=1
                         n_ptr+=1
                     else:
