60d7642ee4299d774eff3d7a56cb7dba67314b8b
Daniel Armas Ramírez <50346756+daniel692a@users.noreply.github.com>
2022-08-24 19:58:35 -0500

    Time: 80 ms (97.25%), Space: 17.8 MB (8.80%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.py
@@ -2,13 +2,14 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.prefixSum = []
+        self.prefixSum = [] # S = O(n)
         sumPrefix = 0
         for i in range(len(self.nums)):
             sumPrefix += self.nums[i]
             self.prefixSum.append(sumPrefix)
 
     def sumRange(self, left: int, right: int) -> int:
+        # O(q)
         if left > 0:
             return self.prefixSum[right]-self.prefixSum[left-1]
         else:
