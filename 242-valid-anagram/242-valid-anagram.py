07ca98f4b804a44737b7a050298a917dd7cc56b6
Daniel Armas Ramírez <50346756+daniel692a@users.noreply.github.com>
2022-09-27 18:24:50 -0500

    Time: 136 ms (9.61%), Space: 14.5 MB (34.84%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        chars1, chars2 = {}, {}
+        for i in range(len(s)):
+            if not (s[i] in chars1):
+                chars1[s[i]]=0
+            else:
+                chars1[s[i]] += 1
+            
+            if not (t[i] in chars2):
+                chars2[t[i]] = 0
+            else:
+                chars2[t[i]] += 1
+        
+        for key in chars1:
+            if key in chars2:
+                if chars2[key] != chars1[key]:
+                    return False
+            else:
+                return False
+        return True
